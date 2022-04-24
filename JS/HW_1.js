6efa46d2b389aab90e14aea8dc03a53cec275aa4
VladislavVoronin <vlad9710@mail.ru>
2022-04-25 02:19:45 +0300

    add new string

--- JS/HW_1.js
+++ JS/HW_1.js
@@ -71,12 +71,17 @@ if (age_1 < age_2) {
 }
 
 // 1*:Преобразовать написанный код в 26-33 пунктах в функцию, принимающую на вход возраст.
+console.log("_________________________________________________________________________________________")
+console.log("Вывести в консоль результат работы функции с возрастами 17, 18, 61")
 const checkAge = function (age) {
     if (age < 18) {console.log("You don’t have access cause your age is " + age + ". It's less then")}
-    else if (age >= 18 & age_1 < 60) {console.log("Welcome")}
-    else if (age >= 60) {console.log("Welcome")}
+    else if (age >= 18 & age < 60) {console.log("Welcome")}
+    else if (age >= 60) {console.log("Technical Work")}
     else {console.log("Technical work")}
 }
 
 checkAge(17)
+checkAge(18)
+checkAge(61)
+
 
