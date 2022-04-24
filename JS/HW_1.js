e892916a6974e516e213f277c1971da206663ecf
VladislavVoronin <vlad9710@mail.ru>
2022-04-25 02:46:37 +0300

    some changes

--- JS/HW_1.js
+++ JS/HW_1.js
@@ -78,7 +78,7 @@ const checkAge = function (age) {
     if (typeof age != "number") {console.log("It's not a number")} 
         else if (age < 18) {console.log("You don’t have access cause your age is " + age + ". It's less then")}
         else if (age >= 18 & age < 60) {console.log("Welcome")}
-        else if (age >= 60) {console.log("Technical Work")}
+        else if (age >= 60) {console.log("Keep calm and look Culture channel")}
         else {console.log("Technical work")} 
     }
 checkAge("hi")
