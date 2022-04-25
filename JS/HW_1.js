7499dd14a817c2888008f810f56115a5fe0d4dbb
VladislavVoronin <vlad9710@mail.ru>
2022-04-25 03:43:39 +0300

    some changes

--- JS/HW_1.js
+++ JS/HW_1.js
@@ -76,13 +76,12 @@ console.log("Вывести в консоль результат работы ф
 // 2*:Преобразовать задание 1* таким образом, чтобы первым делом в функции проверялся тип данных. И если он не Number - кидалась ошибка.
 // 3**Преобразовать 2* таким образом, чтобы значение '2' (строка в которой лежит ТОЛЬКО ЦИФРА) пропускалось, преобразовываясь в number
 const checkAge = function (age) {
-    let num = Number (age); 
-    if (typeof num == "number") { 
-        if (num < 18) {console.log("You don’t have access cause your age is " + num + ". It's less then")}
-        else if (num >= 18 & num < 60) {console.log("Welcome")}
-        else if (num >= 60) {console.log("Keep calm and look Culture channel")}
-        else {console.log("It's not a number")} 
-    }
+    if (typeof Number (age) == "number") { 
+    if (age < 18) {console.log("You don’t have access cause your age is " + age + ". It's less then")}
+    else if (age >= 18 & age < 60) {console.log("Welcome")}
+    else if (age >= 60) {console.log("Keep calm and look Culture channel")}
+    else {console.log("It's not a number")} 
+}
 }
 checkAge("q")
 checkAge(17)
