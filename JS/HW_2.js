28c7fc5d51ab5cf96c0bfea084ca7b9ac792bf1e
VladislavVoronin <vlad9710@mail.ru>
2022-04-29 18:18:29 +0300

    some changes

--- JS/HW_2.js
+++ JS/HW_2.js
@@ -11,35 +11,39 @@
 //  9. Строка не должна быть пустой
 
 function validation (check_string) {
-    if (typeof check_string == "string") {
-        if (/^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/.test(check_string) == true) {
-          console.log("Валидация прошла успешно")
-        } else  {         
-          let isValid = true
-            if (check_string.length < 5) {
-             console.log("Min 5 symbol"), isValid=false
-          } if (check_string.length > 64) {
-             console.log("Max 64 symbol"), isValid=false
-          } if (/[a-zA-Z]/.test(check_string) == false) {
-             console.log("Must be letter in string"), isValid=false
-          } if (/[A-Z]/.test(check_string) == false) {
-             console.log("Must be one uppercase letter"), isValid=false
-          } if (/[0-9]/.test(check_string) == false) {
-             console.log("Must be one number"), isValid=false
-          } if (/\s/.test(check_string) == true) {
-             console.log("String can't contain a space"), isValid=false
-          } if (check_string.includes("@") == false) {
-             console.log("Must be @"), isValid=false
-          } if (check_string.length == 0) {
-             console.log("String can't be empty"), isValid=false
-          } if (isValid == true) {
-             console.log("Example to fill'YourMail2022@meil.com'")
-          }
-        }
-    } else {
-       console.log("It's not a string")
-    };
- }
- 
- 
- validation("ПримерНЕвалидного@МЫЛА+@Q1r.ru")
+   if (typeof check_string == "string") {
+      if (check_string.length == 0) {
+      console.log("String can't be empty")
+      } else {
+         if (/^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/.test(check_string) == true) {         
+         let isValid = true
+           if (check_string.length < 5) {
+            console.log("Min 5 symbol"), isValid=false;
+         } if (check_string.length > 64) {
+            console.log("Max 64 symbol"), isValid=false;
+         } if (/[a-z]/.test(check_string) == false) {
+            console.log("Must be letter in string"), isValid=false;
+         } if (/[A-Z]/.test(check_string) == false) {
+            console.log("Must be one uppercase letter"), isValid=false;
+         } if (/[0-9]/.test(check_string) == false) {
+            console.log("Must be one number"), isValid=false;
+         } if (/\s/.test(check_string) == true) {
+            console.log("String can't contain a space"), isValid=false;
+         } if (check_string.includes("@") == false) {
+            console.log("Must be @"), isValid=false;
+         } if (check_string.length == 0) {
+            console.log("String can't be empty"), isValid=false;
+         } if (isValid == true) {
+            console.log("Validation passed")
+         }
+       } else {
+            console.log("Example to fill 'YourMail2022@meil.com'")
+         } 
+      }
+} else {
+      console.log("It's not a string")
+   }
+
+}
+
+validation("1Qe-mail@mail.ru")
