3774983a918e052a2ce3aa84347fc3d6b46868e2
VladislavVoronin <vlad9710@mail.ru>
2022-05-20 18:35:38 +0300

    add new table

--- SQL/PostgreSQL/HW_2.sql
+++ SQL/PostgreSQL/HW_2.sql
@@ -1,4 +1,4 @@
-п»ї--PostreSQL
+--PostreSQL
 --IP: 159.69.151.133
 --Port: 5056
 --DB: qa_ddl_27_117
@@ -6,25 +6,27 @@
 --Pass: 123
 
 
---РћР±РЅСѓР»РёС‚СЊ Primal Key
---ALTER SEQUENCE salary_id_seq RESTART WITH 1;
+--Обнулить Primal Key
+--ALTER SEQUENCE roles_employee_id_seq RESTART WITH 1;
 --UPDATE salary SET id=nextval('salary_id_seq');
 
---РћС‡РёСЃС‚РёС‚СЊ С‚Р°Р±Р»РёС†Сѓ
---delete from salary;
+--Очистить таблицу
+--delete from roles_employee;
 
---1) РЎРѕР·РґР°С‚СЊ С‚Р°Р±Р»РёС†Сѓ employees
+
+
+
+
+--1) Создать таблицу employees
 --- id. serial,  primary key,
 --- employee_name. Varchar(50), not null
---РќР°РїРѕР»РЅРёС‚СЊ С‚Р°Р±Р»РёС†Сѓ employee 70 СЃС‚СЂРѕРєР°РјРё.
-
-
 
 create table employees (
 id serial primary key,
 employee_name int not null
 );
 
+--2) Наполнить таблицу employee 70 строками.
 
 DO $$
 BEGIN
@@ -36,10 +38,16 @@ end $$;
 
 
 
---2) РЎРѕР·РґР°С‚СЊ С‚Р°Р±Р»РёС†Сѓ salary
+--3) Создать таблицу salary
 --- id. Serial  primary key,
 --- monthly_salary. Int, not null
---РќР°РїРѕР»РЅРёС‚СЊ С‚Р°Р±Р»РёС†Сѓ salary 15 СЃС‚СЂРѕРєР°РјРё:
+
+create table salary (
+id serial primary key,
+monthly_salary int not null
+);
+
+--4) Наполнить таблицу salary 15 строками:
 --- 1000
 --- 1100
 --- 1200
@@ -57,11 +65,6 @@ end $$;
 --- 2400
 --- 2500
 
-create table salary (
-id serial primary key,
-monthly_salary int not null
-);
-
 
 DO $$
 DECLARE salary1 integer := 1000;
@@ -75,18 +78,19 @@ end $$;
 
 
 
---3) РЎРѕР·РґР°С‚СЊ С‚Р°Р±Р»РёС†Сѓ employee_salary
+--5) Создать таблицу employee_salary
 --- id. Serial  primary key,
 --- employee_id. Int, not null, unique
 --- salary_id. Int, not null
---РќР°РїРѕР»РЅРёС‚СЊ С‚Р°Р±Р»РёС†Сѓ employee_salary 40 СЃС‚СЂРѕРєР°РјРё:
---- РІ 10 СЃС‚СЂРѕРє РёР· 40 РІСЃС‚Р°РІРёС‚СЊ РЅРµСЃСѓС‰РµСЃС‚РІСѓСЋС‰РёРµ employee_id
 
 create table employee_salary (
 id serial primary key,
 employee_id int not null unique,
 salary_id int not null
 );
+
+--6)Наполнить таблицу employee_salary 40 строками:
+--- в 10 строк из 40 вставить несуществующие employee_id
 
 DO $$
 BEGIN
@@ -102,7 +106,7 @@ FOR i IN 1..40
 	end loop;
 end $$
 
---4)РЎРѕР·РґР°С‚СЊ С‚Р°Р±Р»РёС†Сѓ roles
+--7)Создать таблицу roles
 --- id. Serial  primary key,
 --- role_name. int, not null, unique
 
@@ -111,8 +115,72 @@ id serial primary key,
 role_name int not null unique
 );
 
---РџРѕРјРµРЅСЏС‚СЊ С‚РёРї СЃС‚РѕР»Р±Р° role_name СЃ int РЅР° varchar(30)
+--8) Поменять тип столба role_name с int на varchar(30)
+
 ALTER TABLE roles ALTER COLUMN role_name TYPE varchar(30);
+
+--9) Наполнить таблицу roles 20 строками:
+
 insert into roles (role_name)
+values	('Junior Python developer'),
+		('Middle Python developer'),
+		('Senior Python developer'),
+		('Junior Java developer'),
+		('Middle Java developer'),
+		('Senior Java developer'),
+		('Junior JavaScript developer'),
+		('Middle JavaScript developer'),
+		('Senior JavaScript developer'),
+		('Junior Manual QA engineer'),
+		('Middle Manual QA engineer'),
+		('Senior Manual QA engineer'),
+		('Project Manager'),
+		('Designer'),
+		('HR'),
+		('CEO'),
+		('Sales manager'),
+		('Junior Automation QA engineer'),
+		('Middle Automation QA engineer'),
+		('Senior Automation QA engineer');
 
+--10)Создать таблицу roles_employee
+--- id. Serial  primary key,
+--- employee_id. Int, not null, unique (внешний ключ для таблицы employees, поле id)
+--- role_id. Int, not null (внешний ключ для таблицы roles, поле id)
+	
+create table roles_employee (
+id serial primary key,
+employee_id int unique not null,
+role_id int not null,
+foreign key(employee_id) references employees(id),
+foreign key(role_id) references roles(id)
+);
 
+--11)Наполнить таблицу roles_employee 40 строками
+
+INSERT INTO roles_employee(employee_id, role_id)
+values ((select id from employees where id =1), (select id from roles where id =1));
+
+DO $$
+DECLARE n integer := 1;
+begin
+FOR i IN 1..40 
+	loop
+		if i <= 20
+			then
+				INSERT INTO roles_employee(employee_id, role_id) 
+				values	(
+						(select id from employees where id = i), 
+						(select id from roles where id = i)
+						);
+		elsif i > 20
+			then 
+				INSERT INTO roles_employee(employee_id, role_id) 
+				values	(
+						(select id from employees where id = i), 
+						(select id from roles where id = n)
+						);
+				n := n + 1;
+		end if;
+	end loop;
+end $$
