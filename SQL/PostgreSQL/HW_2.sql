7fc0f55767d3872bc920e4d549d9c04a49bd6619
VladislavVoronin <vlad9710@mail.ru>
2022-05-20 18:47:18 +0300

    add new table

--- SQL/PostgreSQL/HW_2.sql
+++ SQL/PostgreSQL/HW_2.sql
@@ -1,4 +1,4 @@
-п»ї--PostreSQL
+--PostreSQL
 --IP: 159.69.151.133
 --Port: 5056
 --DB: qa_ddl_27_117
@@ -6,18 +6,18 @@
 --Pass: 123
 
 
---РћР±РЅСѓР»РёС‚СЊ Primal Key
+--Обнулить Primal Key
 --ALTER SEQUENCE roles_employee_id_seq RESTART WITH 1;
 --UPDATE salary SET id=nextval('salary_id_seq');
 
---РћС‡РёСЃС‚РёС‚СЊ С‚Р°Р±Р»РёС†Сѓ
+--Очистить таблицу
 --delete from roles_employee;
 
 
 
 
 
---1) РЎРѕР·РґР°С‚СЊ С‚Р°Р±Р»РёС†Сѓ employees
+--1) Создать таблицу employees
 --- id. serial,  primary key,
 --- employee_name. Varchar(50), not null
 
@@ -26,7 +26,7 @@ id serial primary key,
 employee_name int not null
 );
 
---2) РќР°РїРѕР»РЅРёС‚СЊ С‚Р°Р±Р»РёС†Сѓ employee 70 СЃС‚СЂРѕРєР°РјРё.
+--2) Наполнить таблицу employee 70 строками.
 
 DO $$
 BEGIN
@@ -38,7 +38,7 @@ end $$;
 
 
 
---3) РЎРѕР·РґР°С‚СЊ С‚Р°Р±Р»РёС†Сѓ salary
+--3) Создать таблицу salary
 --- id. Serial  primary key,
 --- monthly_salary. Int, not null
 
@@ -47,7 +47,7 @@ id serial primary key,
 monthly_salary int not null
 );
 
---4) РќР°РїРѕР»РЅРёС‚СЊ С‚Р°Р±Р»РёС†Сѓ salary 15 СЃС‚СЂРѕРєР°РјРё:
+--4) Наполнить таблицу salary 15 строками:
 --- 1000
 --- 1100
 --- 1200
@@ -78,7 +78,7 @@ end $$;
 
 
 
---5) РЎРѕР·РґР°С‚СЊ С‚Р°Р±Р»РёС†Сѓ employee_salary
+--5) Создать таблицу employee_salary
 --- id. Serial  primary key,
 --- employee_id. Int, not null, unique
 --- salary_id. Int, not null
@@ -89,8 +89,8 @@ employee_id int not null unique,
 salary_id int not null
 );
 
---6)РќР°РїРѕР»РЅРёС‚СЊ С‚Р°Р±Р»РёС†Сѓ employee_salary 40 СЃС‚СЂРѕРєР°РјРё:
---- РІ 10 СЃС‚СЂРѕРє РёР· 40 РІСЃС‚Р°РІРёС‚СЊ РЅРµСЃСѓС‰РµСЃС‚РІСѓСЋС‰РёРµ employee_id
+--6)Наполнить таблицу employee_salary 40 строками:
+--- в 10 строк из 40 вставить несуществующие employee_id
 
 DO $$
 BEGIN
@@ -106,7 +106,7 @@ FOR i IN 1..40
 	end loop;
 end $$
 
---7)РЎРѕР·РґР°С‚СЊ С‚Р°Р±Р»РёС†Сѓ roles
+--7)Создать таблицу roles
 --- id. Serial  primary key,
 --- role_name. int, not null, unique
 
@@ -115,11 +115,11 @@ id serial primary key,
 role_name int not null unique
 );
 
---8) РџРѕРјРµРЅСЏС‚СЊ С‚РёРї СЃС‚РѕР»Р±Р° role_name СЃ int РЅР° varchar(30)
+--8) Поменять тип столба role_name с int на varchar(30)
 
 ALTER TABLE roles ALTER COLUMN role_name TYPE varchar(30);
 
---9) РќР°РїРѕР»РЅРёС‚СЊ С‚Р°Р±Р»РёС†Сѓ roles 20 СЃС‚СЂРѕРєР°РјРё:
+--9) Наполнить таблицу roles 20 строками:
 
 insert into roles (role_name)
 values	('Junior Python developer'),
@@ -143,10 +143,10 @@ values	('Junior Python developer'),
 		('Middle Automation QA engineer'),
 		('Senior Automation QA engineer');
 
---10)РЎРѕР·РґР°С‚СЊ С‚Р°Р±Р»РёС†Сѓ roles_employee
+--10)Создать таблицу roles_employee
 --- id. Serial  primary key,
---- employee_id. Int, not null, unique (РІРЅРµС€РЅРёР№ РєР»СЋС‡ РґР»СЏ С‚Р°Р±Р»РёС†С‹ employees, РїРѕР»Рµ id)
---- role_id. Int, not null (РІРЅРµС€РЅРёР№ РєР»СЋС‡ РґР»СЏ С‚Р°Р±Р»РёС†С‹ roles, РїРѕР»Рµ id)
+--- employee_id. Int, not null, unique (внешний ключ для таблицы employees, поле id)
+--- role_id. Int, not null (внешний ключ для таблицы roles, поле id)
 	
 create table roles_employee (
 id serial primary key,
@@ -156,10 +156,7 @@ foreign key(employee_id) references employees(id),
 foreign key(role_id) references roles(id)
 );
 
---11)РќР°РїРѕР»РЅРёС‚СЊ С‚Р°Р±Р»РёС†Сѓ roles_employee 40 СЃС‚СЂРѕРєР°РјРё
-
-INSERT INTO roles_employee(employee_id, role_id)
-values ((select id from employees where id =1), (select id from roles where id =1));
+--11)Наполнить таблицу roles_employee 40 строками
 
 DO $$
 DECLARE n integer := 1;
