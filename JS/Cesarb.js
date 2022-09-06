a1296bd690b76654fd14716633f7ed437e42284d
vlad9710 <vlad9710@mail.ru>
2022-09-07 00:35:13 +0300

    modify Cesarb

--- JS/Cesarb.js
+++ JS/Cesarb.js
@@ -1,15 +1,20 @@
 const Alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 
-function Cesarb (word) {
+function Cesarb (word, shifr) {
     if (/^[a-zA-Z]*$/.test(word) == true) {
         let NewWord = [];
         for (let letter of word.toUpperCase()) {
-            i = Alphabet.indexOf(letter) + 1
+            i = Alphabet.indexOf(letter) + shifr
+            if (i >= Alphabet.length) {
+                i = i - Alphabet.length
+                NewWord.push(Alphabet[i])
+            } else {
             NewWord.push(Alphabet[i])
+            }
         }
         console.log(NewWord.join(''))
     }
 
 }
 
-Cesarb ("Hello")
+Cesarb ("Hello", -3) // (Твое слово, на сколько сдвинуть)
