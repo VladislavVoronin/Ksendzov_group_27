20eff3b0b0960ae03c3daee8a3d5ae1ff095f9f5
vlad9710 <vlad9710@mail.ru>
2022-09-07 09:44:49 +0300

    modify Cesarb

--- JS/Cesarb.js
+++ JS/Cesarb.js
@@ -1,18 +1,18 @@
 const Alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 
-function Cesarb (word, shifr) {
+function cesarb (word, shifr) {
     if (word.length == 0 || shifr.length == 0) {
         console.log("Поле не может быть пустым")
     } else {
         if (/^[a-zA-Z]*$/.test(word) !== true) {
             console.log("Введите слово на английском")
         } else {
-            if (Number(shifr) < -25 || Number(shifr) > 25) {
+            if ((shifr) < -25 || (shifr) > 25) {
                 console.log("Введите значение не менее -25 и не более 25")
             } else {
             let NewWord = [];
             for (let letter of word.toUpperCase()) {
-            i = Alphabet.indexOf(letter) + Number(shifr)
+            i = Alphabet.indexOf(letter) + (shifr)
             if ( i >= 0 && i <= Alphabet.length) {
                 NewWord.push(Alphabet[i])
             } else {
@@ -30,4 +30,4 @@ function Cesarb (word, shifr) {
 } 
 }
 
-Cesarb ("Hello", "-25") // (Твое слово, на сколько сдвинуть(значение от -25 до 25))
+cesarb ("Hello", -20) // (Твое слово, на сколько сдвинуть(значение от -25 до 25))
